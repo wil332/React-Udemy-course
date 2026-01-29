aec8df64f3bff7c0a78b93232d6cad0a517b3dd0
Wilbert <wilbert.blogspot@gmail.com>
2026-01-29 12:34:06 +0700

    working with effect dependencies and useCallback

--- React quiz app/src/components/Quiz.jsx
+++ React quiz app/src/components/Quiz.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState,useCallback } from "react";
 
 import QUESTIONS from '../question';
 
@@ -22,16 +22,18 @@ export default function Quiz(){
     const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
     shuffledAnswers.sort(()=>Math.random()-0.5);
 
-    function handleSelectAnswer(selectedAnswer){
+const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer){
         setUserAnswers((prevUserAnswers)=>{
             return [...prevUserAnswers,selectedAnswer];
         });
-    }
+    },[]);
+
+    const handleSkipAnswer = useCallback(()=>handleSelectAnswer(null),[handleSelectAnswer])
 
     return (<div id="quiz">
 
     <div id="question">
-        <QuestionTimer timeout={10000} onTimeout={()=>handleSelectAnswer(null)}/>
+        <QuestionTimer timeout={10000} onTimeout={handleSkipAnswer}/>
          <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
          <ul id="answers">
             {shuffledAnswers.map((answer)=>(
