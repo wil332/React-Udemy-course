a3d29ea10ee401d47a5ce222cb52eaa8f14fe658
Wilbert <wilbert.blogspot@gmail.com>
2026-01-29 11:57:59 +0700

    deriving values, outputting questions & registering answers

--- React quiz app/src/App.jsx
+++ React quiz app/src/App.jsx
@@ -1,9 +1,12 @@
 import Header from "./components/Header";
+import Quiz from "./components/Quiz";
 
 function App() {
-    return <Header>
-        
-    </Header>
+    return (<>
+    <Header/>
+    <main>
+       <Quiz/>
+    </main>
+    </>)
 }
-
 export default App;
