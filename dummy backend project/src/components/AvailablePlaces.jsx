2f0e32c4b6339e04d75d0784cd9e2924fc174a3f
Wilbert <wilbert.blogspot@gmail.com>
2026-02-01 04:11:41 +0700

    add fetching api

--- dummy backend project/src/components/AvailablePlaces.jsx
+++ dummy backend project/src/components/AvailablePlaces.jsx
@@ -1,6 +1,16 @@
+import { useState,useEffect } from 'react';
 import Places from './Places.jsx';
 
+
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces,setAvailablePlaces] = useState([]);
+
+  useEffect(fetch('https://localhost:3000/places').then((response)=>{
+    return response.json();
+  }).then((resData)=>{
+    setAvailablePlaces(resData.places);
+  }),[])
+
   return (
     <Places
       title="Available Places"
