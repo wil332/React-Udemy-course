3fa8214e71d4c69ebdb953fa4500e00a597eaf88
Wilbert <wilbert.blogspot@gmail.com>
2026-02-06 07:20:49 +0700

    deleting data use http request method

--- dummy backend project/src/App.jsx
+++ dummy backend project/src/App.jsx
@@ -49,9 +49,15 @@ function App() {
     setUserPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current.id)
     );
+    try{
+      await updateUserPlaces(userPlaces.filter((place)=> place.id !== selectedPlace.current.id) );
 
+    } catch(error){
+      setUserPlaces(userPlaces);
+      setErrorUpdatingPlaces({message: error.message || 'Failed to delete place.'})
+    }
     setModalIsOpen(false);
-  }, []);
+  }, [userPlaces]);
 
   function handleError(){
     setErrorUpdatingPlaces(null);
@@ -93,3 +99,5 @@ function App() {
 }
 
 export default App;
+
+
--- dummy backend project/src/http.js
+++ dummy backend project/src/http.js
@@ -9,7 +9,7 @@ export async function fetchAvailablePlaces(){
 }
 
 export async function updateUserPlaces(places){
-    const response = await fetch('http://localhost:3000/user-placesss',{
+    const response = await fetch('http://localhost:3000/user-places',{
         method: 'PUT',
         body: JSON.stringify({places: places}),
         headers:{
