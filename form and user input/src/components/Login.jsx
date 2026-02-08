0946524564e811684d4340e03b29926a1012d781
Wilbert <wilbert.blogspot@gmail.com>
2026-02-08 23:04:59 +0700

    handling input change via managing state and generic handler

--- form and user input/src/components/Login.jsx
+++ form and user input/src/components/Login.jsx
@@ -1,9 +1,24 @@
+import { useState } from "react";
+
 export default function Login() {
   function handleSubmit(event){
     event.preventDefault();
-    console.log('SUBMITTED!');
+    console.log(enteredValues);
     
   }
+
+  const [enteredValues, setEnteredValues] = useState({
+    email: '',
+    password: ''
+  })
+
+  function handleInputChange(identifier, value){
+    setEnteredValues(prevValues=>({
+      ...prevValues,
+      [identifier]: value
+    }))
+  }
+
   return (
     <form onSubmit={handleSubmit}>
       <h2>Login</h2>
@@ -11,12 +26,16 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input id="email" type="email" name="email" 
+          onChange={(event) => handleInputChange('email', event.target.value)}
+          value={enteredValues.email}/>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password" 
+          onChange={(event)=> handleInputChange('password', event.target.value)}
+          value={enteredValues.password}/>
         </div>
       </div>
 
