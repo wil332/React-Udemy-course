b7956fcd08f7c356e66b3f4f67f1944ca26500ab
Wilbert <wilbert.blogspot@gmail.com>
2026-02-08 18:27:15 +0700

    add custom react hook project learning folder

---
+++ Custom React hooks/src/hooks/useFetch.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+
+export default function useFetch(fetchFn,initialValue){
+    const [fetchedData, setFetchedData] = useState(initialValue);
+  const [isFetching, setIsFetching] = useState();
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    async function fetchData() {
+      setIsFetching(true);
+      try {
+        const data = await fetchFn();
+        setFetchedData(data);
+      } catch (error) {
+        setError({ message: error.message || 'Failed to fetch data.' });
+      }
+
+      setIsFetching(false);
+    }
+
+    fetchData();
+  }, [fetchFn]);
+
+  return {
+    isFetching,
+    error,
+    fetchedData
+  }
+}
