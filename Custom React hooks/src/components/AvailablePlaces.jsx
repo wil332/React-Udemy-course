985767974b8e3668b0faf55fc73a898aad4e1416
Wilbert <wilbert.blogspot@gmail.com>
2026-02-08 21:03:16 +0700

    creating flexible custom hooks using promise

--- Custom React hooks/src/components/AvailablePlaces.jsx
+++ Custom React hooks/src/components/AvailablePlaces.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+
 
 import Places from './Places.jsx';
 import Error from './Error.jsx';
@@ -6,8 +6,28 @@ import { sortPlacesByDistance } from '../loc.js';
 import { fetchAvailablePlaces } from '../http.js';
 import useFetch from '../hooks/useFetch.js';
 
+async function fetchSortedPlaces(){
+  const places = await fetchAvailablePlaces();
+
+  return new Promise((resolve)=>{
+    navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            places,
+            position.coords.latitude,
+            position.coords.longitude
+          );
+           resolve(sortedPlaces);
+        }
+  )
+
+ 
+})
+
+ 
+}
+
 export default function AvailablePlaces({ onSelectPlace }) {
-  const {isFetching,error,fetchedData:availablePlaces,setFetchedData:setAvailablePlaces} = useFetch(fetchAvailablePlaces,[])
+  const {isFetching,error,fetchedData:availablePlaces} = useFetch(fetchSortedPlaces,[])
 
   if (error) {
     return <Error title="An error occurred!" message={error.message} />;
