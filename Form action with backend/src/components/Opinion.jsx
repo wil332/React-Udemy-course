aafcd926c918242d4eb1f3c29f0a92e7402ecde1
Wilbert <wilbert.blogspot@gmail.com>
2026-02-09 21:26:39 +0700

    registering formAction to multiple button

--- Form action with backend/src/components/Opinion.jsx
+++ Form action with backend/src/components/Opinion.jsx
@@ -1,4 +1,12 @@
 export function Opinion({ opinion: { id, title, body, userName, votes } }) {
+  function upvoteAction(){
+
+  }
+
+  function downvoteAction(){
+
+  }
+
   return (
     <article>
       <header>
@@ -7,7 +15,7 @@ export function Opinion({ opinion: { id, title, body, userName, votes } }) {
       </header>
       <p>{body}</p>
       <form className="votes">
-        <button>
+        <button formAction={upvoteAction}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
@@ -27,7 +35,7 @@ export function Opinion({ opinion: { id, title, body, userName, votes } }) {
 
         <span>{votes}</span>
 
-        <button>
+        <button formAction={downvoteAction}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
